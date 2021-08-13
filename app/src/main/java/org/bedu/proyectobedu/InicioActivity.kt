fef613655563ce791e23e2998e2d13ad28de4fda
Fernando Torres <fer04.oso@gmail.com>
2021-08-12 22:17:25 -0500

    Se agrego el menu y la barra de herramientas en la pantalla principal y se deberá reubicar ya que no es la ubicación idonea dado el proyecto.

--- app/src/main/java/org/bedu/proyectobedu/InicioActivity.kt
+++ app/src/main/java/org/bedu/proyectobedu/InicioActivity.kt
@@ -3,9 +3,13 @@ package org.bedu.proyectobedu
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.button.MaterialButton
 
 class InicioActivity : AppCompatActivity() {
@@ -34,4 +38,5 @@ class InicioActivity : AppCompatActivity() {
             }
         }
     }
+
 }
--- app/src/main/java/org/bedu/proyectobedu/MainActivity.kt
+++ app/src/main/java/org/bedu/proyectobedu/MainActivity.kt
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button as Button
 import android.content.Intent
+import android.view.Menu
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,10 +21,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val appBar = findViewById<Toolbar>(R.id.app_bar)
+        this.setSupportActionBar(appBar)
+        setupDrawer(appBar)
+
         boton1 = findViewById(R.id.button)
         boton2 = findViewById(R.id.button2)
         textView = findViewById(R.id.forgotpass)
-
 
         boton1.setOnClickListener{
 
@@ -35,5 +43,22 @@ class MainActivity : AppCompatActivity() {
 
             startActivity(Intent(this, ActivityForgotPass::class.java))
         }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun setupDrawer(toolbar: Toolbar) {
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val drawerToggle = ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.open_drawer,
+                R.string.close_drawer
+        )
     }
 }
